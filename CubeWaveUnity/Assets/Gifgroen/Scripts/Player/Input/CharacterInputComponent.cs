dfceb3cd2bc2b10bb1abe611adf62ec925d263b8
Karsten Westra <karstenwestra@gmail.com>
2020-12-02 23:59:19 +0100

    Implemented player movement without turning.

---
+++ CubeWaveUnity/Assets/Gifgroen/Scripts/Player/Input/CharacterInputComponent.cs
@@ -0,0 +1,29 @@
+﻿using Gifgroen.Player.Movement;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Gifgroen.Player.Input
+{
+    public class CharacterInputComponent : MonoBehaviour
+    {
+        [SerializeField] public CharacterMovementComponent movement;
+
+        // ReSharper disable once UnusedMember.Global
+        public void Move(InputAction.CallbackContext c)
+        {
+            Vector2 value = c.ReadValue<Vector2>();
+            if (value.x > 0)
+            {
+                movement.SetMoveRight();
+            }
+            else if (value.x < 0f)
+            {
+                movement.SetMoveLeft();
+            }
+            else
+            {
+                movement.SetStopMove();
+            }
+        }
+    }
+}
---
+++ CubeWaveUnity/Assets/Gifgroen/Scripts/Player/Movement/CharacterMovementComponent.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Gifgroen.Player.Movement
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class CharacterMovementComponent : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody movable;
+
+        [Range(1f, 10f)] [SerializeField] private float maxSpeed = 5f;
+
+        [SerializeField, Range(0f, 100f)] private float maxAcceleration = 64f;
+
+        private Vector3 _desiredVelocity;
+
+        private Vector3 _velocity;
+
+        private Vector3 _moveDirection = Vector3.zero;
+
+        private void OnValidate()
+        {
+            movable = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            _desiredVelocity = _moveDirection * maxSpeed;
+        }
+
+        private void FixedUpdate()
+        {
+            _velocity = movable.velocity;
+            float maxSpeedChange = maxAcceleration * Time.fixedDeltaTime;
+            _velocity.x = Mathf.MoveTowards(_velocity.x, _desiredVelocity.x, maxSpeedChange);
+            _velocity.y = Mathf.MoveTowards(_velocity.y, _desiredVelocity.y, maxSpeedChange);
+            _velocity.z = Mathf.MoveTowards(_velocity.z, _desiredVelocity.z, maxSpeedChange);
+            movable.velocity = _velocity;
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void SetMoveLeft() => SetMoveDirection(Vector3.left);
+
+        // ReSharper disable once UnusedMember.Global
+        public void SetMoveRight() => SetMoveDirection(Vector3.right);
+
+        // ReSharper disable once UnusedMember.Global
+        public void SetStopMove() => SetMoveDirection(Vector3.zero);
+
+        private void SetMoveDirection(Vector3 direction)
+        {
+            _moveDirection = direction;
+        }
+    }
+}
