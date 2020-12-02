0cc2db22f2b02cfd9013f966838a0ef1c5f1d72b
Karsten Westra <karstenwestra@gmail.com>
2020-12-02 13:12:23 +0100

    Added Unity v2019.4.15f1 project with URP and basic scene.

---
+++ CubeWaveUnity/Assets/Gifgroen/Scripts/Core/GameController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Gifgroen.Core
+{
+    public class GameController : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log($"{name}: got Start()");
+        }
+    }
+}
